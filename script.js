e912944b6c0f15d51f74783181b2c1c0b71e92a4
Fatima Achimugu <fatima.achims@gmail.com>
2024-07-13 08:25:36 +0100

    added styling to landing page and some jquery

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 $(document).ready(function(){
-  // $('.fcont-grid3').slick({
-  //   infinite: true,
-  //   slidesToShow: 4,
-  //   slidesToScroll: 4,
-  //   arrows: true
-  // });
+  $('.fcont-grid3').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  });
 
+  // Hover effect
   $('.grid-item').hover(
     function() {
       $(this).find('.front').stop().fadeTo(500, 0);
@@ -16,10 +40,9 @@ $(document).ready(function(){
       $(this).find('.back').stop().fadeTo(500, 0);
     }
   );
-  
-    
-  
-
-  
-  
 });
+
+// var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+// var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//   return new bootstrap.Tooltip(tooltipTriggerEl)
+// });
