40a70d17b416c5f6439bfd340100db3235d5a7fc
Fatima Achimugu <fatima.achims@gmail.com>
2024-07-13 20:15:58 +0100

    styled the registration page

--- register.js
+++ register.js
@@ -1,39 +1,6 @@
 $(document).ready(function(){
-  // Hover effect
-  $('.grid-item').hover(
-    function() {
-      $(this).find('.front').stop().fadeTo(500, 0);
-      $(this).find('.back').stop().fadeTo(500, 1);
-      $(this).find('.cb').show()
-      $(this).find('.c').hide()
-    },
-    function() {
-      $(this).find('.front').stop().fadeTo(500, 1);
-      $(this).find('.back').stop().fadeTo(500, 0);
-      $(this).find('.cb').hide()
-      $(this).find('.c').show()
-    }
-  );
+  
 
-  $('#show').click(function(){
-    $('.options').show()
-  });
-
-  $('.y').hover(
-    function() {
-      $('.x').css('background', '#fff');
-    }, function() {
-      $('.x').css('background', '#0000cdcf');
-    }
-  );
-
-  $('.x').hover(
-    function() {
-      $('.y').css('background', '#fff');
-    }, function() {
-      $('.y').css('background', '#0000cdcf');
-    }
-  );
 
 });
 
